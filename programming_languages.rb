6abefda80697b28a183182037c3cd9eb626f1563
Tommy Lau <tlau425@yahoo.com>
2019-03-07 01:30:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,14 @@
 def reformat_languages(languages)
-  new_hash = {}
+  new_languages = {}
   languages.each do |style, language|
-    language.each do |type, style|
-       new_hash[languages] ||= style
-      new_hash[languages][:style] << type
-
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
     end
   end
-  new_hash
+  new_languages[:javascript][:style] << :oo
+  new_languages
 end
