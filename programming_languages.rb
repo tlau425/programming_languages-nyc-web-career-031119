ffc7e3a550b22441ec745ed284844460fcce5450
Tommy Lau <tlau425@yahoo.com>
2019-03-07 01:28:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     language.each do |type, style|
-      new_hash[languages] [type]= style
-      new_hash[languages] [style] = type
+      new_hash[languages][type] << style
+      new_hash[languages][style] << type
     end
   end
   new_hash
