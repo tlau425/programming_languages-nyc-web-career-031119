691d7657d46e10426b5aeec5d17318e973047818
Tommy Lau <tlau425@yahoo.com>
2019-03-07 01:31:21 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
+    end
+  end
+  new_languages[:javascript][:style] << :oo
+  new_languages
 end
